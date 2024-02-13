0f257e9c8cdd5463234b797022944216822c00d1
Sherif Fouad <130251496+Sheriff30@users.noreply.github.com>
2024-02-13 10:09:41 +0200

    updates

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 const winscontent = document.querySelector(".wins-content");
 const scrollerinner = document.querySelector(".scroller_inner");
+const croosIcon = document.querySelector(".cross-icon");
+const menubarIcon = document.querySelector(".menubar");
+const menubarPage = document.querySelector(".menubarPage");
 
+// Menubar page
+
+menubarIcon.addEventListener("click", function () {
+  menubarPage.classList.add("showMenubarPage");
+  document.body.classList.add("overFlowHidden");
+});
+croosIcon.addEventListener("click", function () {
+  menubarPage.classList.remove("showMenubarPage");
+  document.body.classList.remove("overFlowHidden");
+});
+
+// Gallery Section
 if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
   addAnimation();
 }
