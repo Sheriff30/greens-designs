841957286143c31917a83687b1271a2b4fcff5af
Sherif Fouad <130251496+Sheriff30@users.noreply.github.com>
2024-02-14 22:03:19 +0200

    updates

--- script.js
+++ script.js
@@ -16,16 +16,43 @@ croosIcon.addEventListener("click", function () {
 });
 
 // Gallery Section
-if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
-  addAnimation();
+if (winscontent) {
+  if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
+    addAnimation();
+  }
+
+  function addAnimation() {
+    winscontent.setAttribute("data-animated", true);
+    scrollerinnerContent = Array.from(scrollerinner.children);
+    scrollerinnerContent.forEach((i) => {
+      const duplicatedItem = i.cloneNode(true);
+      duplicatedItem.setAttribute("aria-hidden", true);
+      scrollerinner.appendChild(duplicatedItem);
+    });
+  }
+}
+// WORKS SECTION
+
+const categories = document.querySelectorAll(".category");
+const galleries = document.querySelectorAll(".category-gallery");
+
+function handleCategoryClick(event) {
+  const selectedCategory = event.target.dataset.category;
+
+  galleries.forEach((gallery) => {
+    if (gallery.classList.contains(selectedCategory)) {
+      gallery.classList.remove("hidden");
+    } else {
+      gallery.classList.add("hidden");
+    }
+  });
+
+  categories.forEach((category) => {
+    category.classList.remove("active-category");
+  });
+  event.target.classList.add("active-category");
 }
 
-function addAnimation() {
-  winscontent.setAttribute("data-animated", true);
-  scrollerinnerContent = Array.from(scrollerinner.children);
-  scrollerinnerContent.forEach((i) => {
-    const duplicatedItem = i.cloneNode(true);
-    duplicatedItem.setAttribute("aria-hidden", true);
-    scrollerinner.appendChild(duplicatedItem);
-  });
-}
+categories.forEach((category) => {
+  category.addEventListener("click", handleCategoryClick);
+});
